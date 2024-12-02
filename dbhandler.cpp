8cc5a1c46292078cf8caaed12e5c1409c45b2445
frostie <mattwiner04@gmail.com>
2024-12-02 15:16:32 -0800

    3. Updated Git Ignore

--- dbhandler.cpp
+++ dbhandler.cpp
@@ -1,3 +1,54 @@
 #include "dbhandler.h"
+#include <QString>
+#include <QDebug>
+//Constructor
+dbHandler::dbHandler() {
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("trips.db");
+    if (!db.open()){
+        qDebug() << "Error Connection Failed";
+    } else {
+        qDebug() << "Succsess";
+    }
+}
+//Destructor
+dbHandler::~dbHandler(){
+    if (db.isOpen()){
+        db.close();
+        qDebug() << "DB Closed";
+    }
+}
+//Create tr
+bool dbHandler::createTripTable(){
+    QSqlQuery query;
+    QString createTableQuery =
+        "CREATE TABLE IF NOT EXISTS trips ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "tripName TEXT, "
+        "startDate TEXT, "
+        "endDate TEXT, "
+        "peopleCount INTEGER)";
+    if (!query.exec(createTableQuery)){
+        qDebug() << "Failed to create table" << query.lastError().text();
+        return false;
+    }
+    return true;
+}
 
-dbHandler::dbHandler() {}
+//get all trips from the db
+QSqlQuery dbHandler::getTrips(){
+    QSqlQuery query("SELECT * FROM trips");
+    if (!query.exec()){
+        qDebug() << "Failed to fetch trips" << query.lastError().text();
+    }
+    return query;
+}
+
+// Set current trip
+void dbHandler::setTripName(const QString &name)const{
+    tripName = name;
+}
+// Get the current trip name
+void dbHandler:getTripName() const {
+    return tripName;
+}
--- dbhandler.h
+++ dbhandler.h
@@ -2,14 +2,28 @@
 #define DBHANDLER_H
 
 #include <QObject>
-
-class dbHandler
-{
+#include <QSqlDatabase>
+#include <QSqlQuery>
+class dbHandler {
 public:
     dbHandler();
+    ~dbHandler();
+    //Create a table for the trip
     bool createTripTable();
-    bool addTrip(const QString &tripName, const QString &startDate, const QString &endDate, int peopleCount);
+    bool addTrip(
+        const QString &TripName,
+        const QString &startDate,
+        const QString &endDate,
+        int peopleCount
+        );
 
+    QSqlQuery getTrips();
+
+    void setTripName(const QString &name);
+    QString getTripName() const;
+private:
+    QSqlDatabase db;
+    QString tripName;
 };
 
 #endif // DBHANDLER_H
